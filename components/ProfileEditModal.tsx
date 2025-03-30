dc55eeeed74d569fe26c6b23a0d5bf566b447f57
Reiwilo8 <kddfat@jadamspam.pl>
2025-03-30 16:44:22 +0200

    Implemented ProfileEditModal dynamic color scheme

--- components/ProfileEditModal.tsx
+++ components/ProfileEditModal.tsx
@@ -3,6 +3,8 @@ import { Modal, View, Text, StyleSheet, TouchableWithoutFeedback } from 'react-n
 import { Divider } from './ui/Divider';
 import { SubmitButton } from './button/SubmitButton';
 
+import { useThemeColor } from '@/hooks/useThemeColor';
+
 interface ProfileEditModalProps {
   title?: string;
   isVisible: boolean;
@@ -22,13 +24,16 @@ const ProfileEditModal: React.FC<ProfileEditModalProps> = ({
   isSelector = false,
   isSubmit = true,
 }) => {
+  const backgroundColor = useThemeColor({}, 'background');
+  const textColor = useThemeColor({}, 'text');
+
   return (
     <Modal animationType='fade' transparent visible={isVisible} onRequestClose={closeModal} >
       <TouchableWithoutFeedback onPress={closeModal} >
         <View style={styles.modalOverlay}>
           <TouchableWithoutFeedback onPress={() => { }} >
-            <View style={styles.modalContent} >
-              <Text style={styles.modalTitleText}>{title}</Text>
+            <View style={[styles.modalContent, { backgroundColor: backgroundColor }]} >
+              <Text style={[styles.modalTitleText, { color: textColor }]}>{title}</Text>
               <Divider />
               <View style={[ styles.modalChildrenContainer, { marginTop: isSelector ? 0 : 10 } ]}>
                 {children}
@@ -57,7 +62,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: '70%',
     maxHeight: '70%',
-    backgroundColor: '#000000',
     borderRadius: 10,
     borderColor: '#757575',
     borderWidth: 1,
@@ -68,7 +72,6 @@ const styles = StyleSheet.create({
     fontFamily: 'Roboto',
     fontSize: 20,
     fontWeight: 400,
-    color: '#FFFFFF',
     marginBottom: 10,
   },
   modalChildrenContainer: {
